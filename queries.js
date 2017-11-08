13e0c137dabfd5c5ec970d9d15507438c671e578
allyface <ally.b.kendall@gmail.com>
2017-11-07 16:10:10 -0800

    create removePuppy query

--- queries.js
+++ queries.js
@@ -73,10 +73,27 @@ function updatePuppy(req, res, next) {
     });
 }
 
+function removePuppy(req, res, next) {
+  var pupID = parseInt(req.params.id);
+  db.result('delete from pups where id = $1', pupID)
+    .then(function (result) {
+      /* jshint ignore:start */
+      res.status(200)
+        .json({
+          status: 'success',
+          message: `Removed ${result.rowCount} puppy`
+        });
+      /* jshint ignore:end */
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
 module.exports = {
   getAllPuppies: getAllPuppies,
   getSinglePuppy: getSinglePuppy,
   createPuppy: createPuppy,
-  updatePuppy: updatePuppy
-  // removePuppy: removePuppy
+  updatePuppy: updatePuppy,
+  removePuppy: removePuppy
 };
