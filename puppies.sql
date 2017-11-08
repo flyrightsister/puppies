83548ca9a6003bdd002e57a9205c583a97eebad3
allyface <ally.b.kendall@gmail.com>
2017-11-07 16:41:28 -0800

    update schema to include foreign key

--- puppies.sql
+++ puppies.sql
@@ -3,21 +3,18 @@ DROP DATABASE IF EXISTS puppies;
 
 \c puppies;
 
-CREATE TABLE pups (
-          ID SERIAL PRIMARY KEY,
-        name VARCHAR,
-    breed_ID VARCHAR,
-         age INTEGER,
-         sex VARCHAR
-);
-
 CREATE TABLE breeds (
     breed_ID SERIAL PRIMARY KEY,
   breed_name VARCHAR
 );
 
-INSERT INTO pups (name, breed_ID, age, sex)
-     VALUES ('Tyler', 'Retriever', 3, 'M');
+CREATE TABLE pups (
+          ID SERIAL PRIMARY KEY,
+        name VARCHAR,
+    breed_ID INTEGER REFERENCES breeds,
+         age INTEGER,
+         sex VARCHAR
+);
 
 INSERT INTO breeds (breed_name)
      VALUES ('Mutt'),
@@ -26,3 +23,6 @@ INSERT INTO breeds (breed_name)
             ('Retriever'),
             ('annoying')
 ;
+
+INSERT INTO pups (name, breed_ID, age, sex)
+     VALUES ('Tyler', 4, 3, 'M');
