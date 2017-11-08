4015faa5bd2d416eed49dad4117490db38580efd
allyface <ally.b.kendall@gmail.com>
2017-11-07 16:25:22 -0800

    create puppy breed schema

--- puppies.sql
+++ puppies.sql
@@ -6,10 +6,23 @@ DROP DATABASE IF EXISTS puppies;
 CREATE TABLE pups (
           ID SERIAL PRIMARY KEY,
         name VARCHAR,
-       breed VARCHAR,
+    breed_ID VARCHAR,
          age INTEGER,
          sex VARCHAR
 );
 
-INSERT INTO pups (name, breed, age, sex)
+CREATE TABLE breeds (
+    breed_ID SERIAL PRIMARY KEY,
+  breed_name VARCHAR
+);
+
+INSERT INTO pups (name, breed_ID, age, sex)
      VALUES ('Tyler', 'Retriever', 3, 'M');
+
+INSERT INTO breeds (breed_name)
+     VALUES ('Mutt'),
+            ('Shih-tzu'),
+            ('Pit Bull'),
+            ('Retriever'),
+            ('annoying')
+;
